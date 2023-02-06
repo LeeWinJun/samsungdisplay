16d322ad521d38c2c3c017a19230042081ec2d30
happyna97 <119560642+happyna97@users.noreply.github.com>
2023-02-06 14:59:25 +0900

    Update tech.js

--- js/tech.js
+++ js/tech.js
@@ -1,34 +1,51 @@
-// oled panner 스크롤에 따라 펼쳐지는 것
 let $window = $(window);
-let oled = $window.innerHeight();
-let oledLayer = $('.oled-layer-list');
-let oledLayerTop = oledLayer.offset().top;
 let scrollAmt = $window.scrollTop();
-$window.scroll(function() {
-    if(scrollAmt >= oledLayerTop/2){
-        $('.oled-layer-list').addClass('unfold');
-    }else{
-        $('.oled-layer-list').removeClass('unfold');
-    }
+
+// orange video section
+let Video = $(".key_expression video");
+let VideoTop = Video.offset().top;
+$window.scroll(function () {
+  if (scrollAmt > VideoTop) {
+  }
 });
+
+// function play(){
+//     video.get(0).currentTime = 0;
+//     video.get(0).play();
+//   }
+//   function pause(){
+//     video.get(0).pause();
+//   }
+//   function stop(){
+//     video.get(0).pause();
+//     video.get(0).currentTime = 0;
+//   }
+
+// orange video key expression section
+// oled panner 스크롤에 따라 펼쳐지는 것
+// let oled = $window.innerHeight();
+// let oledLayer = $(".oled-layer-list");
+// let oledLayerTop = oledLayer.offset().top;
+// $window.scroll(function () {
+//   if (scrollAmt >= oledLayerTop / 2) {
+//     $(".oled-layer-list").addClass("unfold");
+//   } else {
+//     $(".oled-layer-list").removeClass("unfold");
+//   }
+// });
 
 // 다크모드 스크롤에 따라 white에서 black화면으로 바뀌는 것
-let fold
-let foldingList = $('.folding-img-list')
-let foldingListTop = foldingList.offset().top;
-$window.scroll(function() {
-if(scrollAmt >= foldingListTop/2){
-    $(".folding-img-item").addClass("on");
-}else{
-    $(".folding-img-item").removeClass("on");
-}
+let darkmord = $(".darkmode");
+let darkmordTop = darkmord.offset().top;
+let blackWindow = $(".darkmode .laptop_wrap .laptop_black");
+console.log(blackWindow);
+$window.scroll(function () {
+  if (scrollAmt >= darkmordTop) {
+    blackWindow.css({ left: "0" });
+  } else {
+    blackWindow.css({ left: "-100%" });
+  }
 });
 
-
-    $(".folding-img-item").each(function(){
-    var imagePos = $(this).offset().top;
-    var topOfWindow = $(window).scrollTop();
-    if (imagePos < topOfWindow+400) {
-        $(this).addClass("fadeIn");
-    }
-    });
+// folderble 스크롤에 따라서 화면이 접히는 것
+// flex device 스크롤에 따라서 위 아래로 이동하는
