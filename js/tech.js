49c05f80d545cfa8e1608d7e21843a01dc6c465a
happyna97 <119560642+happyna97@users.noreply.github.com>
2023-02-08 13:22:00 +0900

    tech 제이쿼리
    tech 제이쿼리

--- js/tech.js
+++ js/tech.js
@@ -1,51 +1,90 @@
+AOS.init();
+var s = skrollr.init();
+
+// 변수
 let $window = $(window);
-let scrollAmt = $window.scrollTop();
-
-// orange video section
+let expression = $(".key_expression");
 let Video = $(".key_expression video");
-let VideoTop = Video.offset().top;
-$window.scroll(function () {
-  if (scrollAmt > VideoTop) {
-  }
-});
-
-// function play(){
-//     video.get(0).currentTime = 0;
-//     video.get(0).play();
-//   }
-//   function pause(){
-//     video.get(0).pause();
-//   }
-//   function stop(){
-//     video.get(0).pause();
-//     video.get(0).currentTime = 0;
-//   }
-
-// orange video key expression section
-// oled panner 스크롤에 따라 펼쳐지는 것
-// let oled = $window.innerHeight();
-// let oledLayer = $(".oled-layer-list");
-// let oledLayerTop = oledLayer.offset().top;
-// $window.scroll(function () {
-//   if (scrollAmt >= oledLayerTop / 2) {
-//     $(".oled-layer-list").addClass("unfold");
-//   } else {
-//     $(".oled-layer-list").removeClass("unfold");
-//   }
-// });
-
-// 다크모드 스크롤에 따라 white에서 black화면으로 바뀌는 것
+let expressionTop = expression.offset().top - 500;
+let oledFlex = $(".self_luminous .oled_flex");
+let oledFlexTop = oledFlex.offset().top - 500;
+let oledItem = $(".oled-layer-item");
 let darkmord = $(".darkmode");
 let darkmordTop = darkmord.offset().top;
-let blackWindow = $(".darkmode .laptop_wrap .laptop_black");
-console.log(blackWindow);
+let blackWindow = $(".darkmode .laptop_wrap figure");
+let folderble = $(".folding-area");
+let folderbleTop = folderble.offset().top - 500;
+let foldingItem = $(".folding-img-item");
+let excuted = false;
+
+//overlay menu
+let menuBtn = document.querySelector(".menu-btn"),
+  closeBtn = document.querySelector(".close-btn"),
+  overlayMenu = document.querySelector(".overlay-menu"),
+  bodyScrollNone = document.querySelector("body");
+
+menuBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  overlayMenu.classList.add("active");
+  bodyScrollNone.classList.add("scroll-none");
+});
+
+closeBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  overlayMenu.classList.remove("active");
+  bodyScrollNone.classList.remove("scroll-none");
+});
+
 $window.scroll(function () {
-  if (scrollAmt >= darkmordTop) {
-    blackWindow.css({ left: "0" });
+  let scrollAmt = $window.scrollTop();
+  // orange video section
+  if (scrollAmt > expressionTop) {
+    Video.addClass("active");
+    Video.get(0).play();
   } else {
-    blackWindow.css({ left: "-100%" });
+    Video.removeClass("active");
+    Video.get(0).pause();
+    Video.get(0).currentTime = 0;
+  }
+  // OLED panner layer
+  console.log("scrollAmt", scrollAmt);
+  console.log("oledFlexTop", oledFlexTop);
+  if (scrollAmt > oledFlexTop) {
+    console.log("true");
+    oledItem.filter(".nth-3").stop().animate({ left: "68px" }, 500);
+    oledItem.filter(".nth-2").stop().animate({ left: "136px" }, 500);
+    oledItem.filter(".nth-1").stop().animate({ left: "204px" }, 500);
+    oledItem.find("p").css({ opacity: "1" });
+  } else {
+    oledItem.filter(".nth-3").stop().animate({ left: "25%" }, 500);
+    oledItem.filter(".nth-2").stop().animate({ left: "50%" }, 500);
+    oledItem.filter(".nth-1").stop().animate({ left: "75%" }, 500);
+    oledItem.find("p").css({ opacity: "0" });
+  }
+  // Darkmode section_from white to black
+  if (scrollAmt > darkmordTop) {
+    blackWindow.addClass("active");
+  } else {
+    blackWindow.removeClass("active");
+  }
+  // folderble
+  if (scrollAmt > folderbleTop) {
+    if (excuted == false) {
+      showFolding();
+      excuted = true;
+    }
+  } else {
+    excuted = false;
   }
 });
 
-// folderble 스크롤에 따라서 화면이 접히는 것
-// flex device 스크롤에 따라서 위 아래로 이동하는
+// folderble(함수)
+function showFolding() {
+  let i = 0;
+  setInterval(() => {
+    if (foldingItem.length > i) {
+      foldingItem.removeClass("on");
+      foldingItem.eq(i++).addClass("on");
+    }
+  }, 200);
+}
