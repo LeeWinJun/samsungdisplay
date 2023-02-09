564490bcc21548734266fc35146685a651ef7fca
HyeonJinSon <119560821+HyeonJinSon@users.noreply.github.com>
2023-02-09 15:43:17 +0900

    Update center.js

--- js/center.js
+++ js/center.js
@@ -1,31 +1,31 @@
 /* ----- TAB ----- */
 
 let tabMenu = $(".tab-menu li"),
-  tabMap = $(".map_img img"),
-  tabContent = $("#tab-content > div");
+    tabMap = $(".map_img img"),
+    tabContent = $("#tab-content > div");
 
 tabContent.hide();
 tabContent.eq(0).show();
 
 tabMenu.click(function (e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  let targetIdx = $(this).index();
+    let targetIdx = $(this).index();
 
-  tabMenu.removeClass("active");
-  $(this).addClass("active");
+    tabMenu.removeClass("active");
+    $(this).addClass("active");
 
-  tabContent.hide();
-  tabContent.eq(targetIdx).show();
-  tabContent.removeClass("active");
-  tabContent.eq(targetIdx).addClass("active");
+    tabContent.hide();
+    tabContent.eq(targetIdx).show();
+    tabContent.removeClass("active");
+    tabContent.eq(targetIdx).addClass("active");
 
-  tabMap.removeClass("active");
-  tabMap.eq(targetIdx).addClass("active");
+    tabMap.removeClass("active");
+    tabMap.eq(targetIdx).addClass("active");
 
-  //탭 클릭 시 이전 탭의 아코디언 해제
-  $(".center_accordion .center_map").slideUp();
-  $(".center_accordion .center_name").removeClass("active");
+  //탭 클릭 시 이전 탭 아코디언 닫기
+    $(".center_accordion .center_map").slideUp();
+    $(".center_accordion .center_name").removeClass("active");
 });
 
 /* ----- ACCORDION ----- */
@@ -33,15 +33,15 @@ tabMenu.click(function (e) {
 let centerWarp = $(".center_accordion");
 
 centerWarp.each(function () {
-  let title = $(this).find(".center_name");
+    let title = $(this).find(".center_name");
 
-  title.click(function () {
-    $(this).next().slideToggle();
-    $(this).parent("li").siblings().find(".center_map").slideUp();
+    title.click(function () {
+        $(this).next().slideToggle();
+        $(this).parent("li").siblings().find(".center_map").slideUp();
 
-    $(this).toggleClass("active");
-    $(this).parent("li").siblings().find(".center_name").removeClass("active");
-  });
+        $(this).toggleClass("active");
+        $(this).parent("li").siblings().find(".center_name").removeClass("active");
+    });
 });
 
 /* ----- MAP-LOAD ----- */
@@ -53,7 +53,6 @@ let markers = [];
 function initMap() {
   let $maps = $(".center_map");
   $.each($maps, function (i, value) {
-    //console.log("lat: "+$(value).attr('data-lat'));
     let coordinate = {
       lat: parseFloat($(value).attr("data-lat")),
       lng: parseFloat($(value).attr("data-lng")),
@@ -76,27 +75,27 @@ function initMap() {
 
 /* ----- header scroll ----- */
 
-let offsetTop = $("#tab-content").offset().top;
+let offsetTop = $('#tab-content').offset().top;
 header = $("header");
 menuText_1 = header.find($(".menu_title p"));
 menuText_2 = header.find($(".menu_title a"));
-function bannerWhite() {
-  $(".sub-title").css({ color: "#fff" });
-  header.css({ color: "#fff" });
-  menuText_1.css({ color: "#fff" });
-  menuText_2.css({ color: "#fff" });
-}
+function bannerWhite (){
+    $(".sub-title").css({ color: "#fff" });
+    header.css({ color: "#fff" });
+    menuText_1.css({ color: "#fff" });
+    menuText_2.css({ color: "#fff" });
+};
 bannerWhite();
 $window.scroll(function () {
-  console.log(offsetTop);
-  console.log($(this).scrollTop());
-  //header color change
-  if ($(this).scrollTop() >= offsetTop) {
-    $(".sub-title").css({ color: "#333" });
-    header.css({ color: "#333" });
-    menuText_1.css({ color: "#333" });
-    menuText_2.css({ color: "#333" });
-  } else {
-    bannerWhite();
-  }
-});
+    console.log(offsetTop);
+    console.log($(this).scrollTop());
+    //header color change
+    if ($(this).scrollTop() >= offsetTop) {
+      $(".sub-title").css({ color: "#333" });
+      header.css({ color: "#333" });
+      menuText_1.css({ color: "#333" });
+      menuText_2.css({ color: "#333" });
+    } else {
+        bannerWhite();
+    }
+  });
