4d3854a58265a76e25d10f464af7a6cc6b6b7f47
happyna97 <119560642+happyna97@users.noreply.github.com>
2023-02-10 00:58:32 +0900

    Update common.js

--- js/common.js
+++ js/common.js
@@ -47,6 +47,41 @@ $(window).scroll(function () {
   lastScrollTop = scrollTop;
 });
 
+/* ----- HEADER SCROLL color change ----- */
+
+let offsetTop = $(".banner").innerHeight();
+header = $("header");
+menuText_1 = header.find($(".menu_title p"));
+menuText_2 = header.find($(".menu_title a"));
+function bannerWhite() {
+    $(".sub-title").css({ color: "#fff" });
+    header.css({ color: "#fff" });
+    menuText_1.css({ color: "#fff" });
+    menuText_2.css({ color: "#fff" });
+}
+bannerWhite();
+$window.scroll(function () {
+    //header color change
+    if ($(this).scrollTop() >= offsetTop) {
+        if (header.hasClass("black")) {
+            $(".sub-title").css({ color: "#fff" });
+            header.css({ color: "#fff" });
+            menuText_1.css({ color: "#fff" });
+            menuText_2.css({ color: "#fff" });
+            header.css({ background: "#000" });
+        } else {
+            $(".sub-title").css({ color: "#333" });
+            header.css({ color: "#333" });
+            menuText_1.css({ color: "#333" });
+            menuText_2.css({ color: "#333" });
+            header.css({ background: "#fff" });
+        }
+    } else {
+        bannerWhite();
+        header.css({ background: "rgba(174, 174, 174, 0.2)" });
+    }
+});
+
 /* ----- GO TO TOP ----- */
 
 let $btt = $("#go-top");
