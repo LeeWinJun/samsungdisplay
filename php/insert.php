40a4a78777dd6feab75a887c17d6189a15a4a3d9
redseon <119560806+redseon@users.noreply.github.com>
2023-02-09 14:28:15 +0900

    Add files via upload

--- php/insert.php
+++ php/insert.php
@@ -28,16 +28,11 @@
     //       type : {$type}";
 
 
-    // $sql = "INSERT INTO inquire_board_2nd(username, phone, email, company, title, content, nation_lang, news_chk, type)
-    //         VALUES('테스트', '123', 'abc@abc.com', 'test', 'test', 'test', 'Belgium / French', 0, 1)";
 
-    //insert가 안되는건 news_chk에 값이 없어서 그런거 같아
-    //체크를 안하면 news_chk :     << 아무것도 없는 값이 들어와서
 
     $sql = "INSERT INTO inquire_board_2nd(username, phone, email, company, title,content,nation_lang, news_chk, type)
             VALUES('{$username}', '{$phone}', '{$email}', '{$company}', '{$title}', '{$content}', '{$nation_lang}', {$news_chk}, {$type})"; 
             //VALUES('{$username}', '{$phone}', '{$email}', '{$company}', '{$title}', '{$content}', '{$nation_lang}', , {$type})
-            // 이렇게 되니까 에러 발생하는거야 체크 안하고 insert하면 오류나느거 확인하면 돼 
 
     $result = mysqli_query($conn, $sql);
     
