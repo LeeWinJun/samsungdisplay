80571928b1b3b3f7535fbb2de3a66b1a992399ef
redseon <119560806+redseon@users.noreply.github.com>
2023-02-10 01:58:16 +0900

    Add files via upload

--- js/service.js
+++ js/service.js
@@ -25,22 +25,18 @@ inquireBtn.click(function () {
     /*클래스명 on 일때 data-type의 값을 가져온다 */
     let dataType = $(".service_tab .on").attr("data-type");
     location.href = `./service_inquire.html?type=${dataType}`;
-    // if (dataType == 1) {
-    //   alert("'제품' 문의페이지로 이동합니다.");
-    // } else {
-    //   alert("'기타' 문의페이지로 이동합니다.");
-    // }
   } else {
     $("#agreeMsg").css({ visibility: "visible" }); //alert 대신!
   }
 });
 
 //header color change
-let offsetTop = $(".service").offset().top;
+let offsetTop1 = $(".service").offset().top;
 header = $("header");
 menuText_1 = header.find($(".menu_title p"));
 menuText_2 = header.find($(".menu_title a"));
 function bannerWhite() {
+  $(".icon-menu div").css({ background: "#fff" });
   $(".sub-title").css({ color: "#fff" });
   header.css({ color: "#fff" });
   menuText_1.css({ color: "#fff" });
@@ -49,11 +45,12 @@ function bannerWhite() {
 bannerWhite();
 
 $window.scroll(function () {
-  console.log(offsetTop);
+  console.log(offsetTop1);
   console.log($(this).scrollTop());
   //header color change
-  if ($(this).scrollTop() >= offsetTop) {
+  if ($(this).scrollTop() >= offsetTop1) {
     $(".sub-title").css({ color: "#333" });
+    $(".icon-menu div").css({ background: "#333" });
     header.css({ color: "#333" });
     menuText_1.css({ color: "#333" });
     menuText_2.css({ color: "#333" });
